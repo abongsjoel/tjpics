c19782a4a636b55d274304c39e59e330b8877990
Joel Chi <abongsjoel@gmail.com>
2024-10-22 01:24:21 +0100

    Get list of images on pressing enter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+
 import SearchBar from "./components/SearchBar";
+import searchImages from "./api";
 
 function App() {
-  const handleSubmit = (term) => {
-    console.log({ term });
+  const handleSubmit = async (term) => {
+    const results = await searchImages(term);
+    console.log({ results });
   };
 
   return (
